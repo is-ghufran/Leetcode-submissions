df2ce6681d9d493883f42af3d2538658994de47d
is_ghufran <146336837+is-ghufran@users.noreply.github.com>
2026-05-12 19:25:19 +0530

    Add: longest-consecutive-sequence - submission-0

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        sort(nums.begin(), nums.end());
+        int longest = 1;
+        int streak = 1;
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] == nums[i - 1]) {
+                continue;
+            }
+            else if(nums[i] == nums[i - 1] + 1) {
+                streak++;
+            }
+            else {
+                streak = 1;
+            }
+            longest = max(longest, streak);
+        }
+        return longest;
+    }
+};
